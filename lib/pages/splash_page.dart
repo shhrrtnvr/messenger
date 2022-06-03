573b9b53496ebfe1e8105fbed0eb185b4de38503
shahriar tanvir <shhrrtnvr@gmail.com>
2022-06-03 13:19:52 +0600

    flutter core initialization

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
@@ -15,6 +16,14 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _setup().then(
+      (_) => widget.onInitializationComplete(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,4 +46,12 @@ class _SplashPageState extends State<SplashPage> {
           ),
         )));
   }
+
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  void _registerServices() {}
 }
