b7523d70692899b6eec0fc92859d0dc6e82d5778
shahriar tanvir <shhrrtnvr@gmail.com>
2022-06-03 14:58:13 +0600

    login page background

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,12 +8,45 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late double _deviceHeigth;
+  late double _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeigth = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03,
+          vertical: _deviceHeigth * 0.02,
+        ),
+        height: _deviceHeigth * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _pageTitle(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return Container(
+      height: _deviceHeigth * 0.10,
+      child: const Text(
+        'Messenger',
+        style: TextStyle(
+            color: Colors.white, fontSize: 40, fontWeight: FontWeight.w600),
+      ),
+    );
   }
 }
