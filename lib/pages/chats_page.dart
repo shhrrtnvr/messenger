d1c599790133141abbdc12ca6d1241c846d98431
shahriar tanvir <shhrrtnvr@gmail.com>
2022-06-04 15:46:34 +0600

    homepage topbar

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -1,6 +1,13 @@
+//Packages
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
+
+//Providers
+import '../providers/authentication_provider.dart';
+
+//Widgets
+import '../widgets/top_bar.dart';
 
 class ChatsPage extends StatefulWidget {
   @override
@@ -10,14 +17,46 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return const Scaffold(
-      backgroundColor: Colors.red,
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            "Charts",
+            primaryAction: IconButton(
+              icon: const Icon(
+                Icons.logout,
+                color: Color.fromRGBO(0, 82, 218, 1.0),
+              ),
+              onPressed: () {
+                _auth.logout();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
