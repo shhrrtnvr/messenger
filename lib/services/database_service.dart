d3b76b866b911c311890f04981de3320e68e75e1
shahriar tanvir <shhrrtnvr@gmail.com>
2022-06-05 03:20:24 +0600

    chat db methods in database service

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,6 +1,9 @@
 //Packages
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//Models
+import '../models/chat_message.dart';
+
 //Constants
 import '../constants/db_fields.dart';
 
@@ -46,6 +49,41 @@ class DatabaseService {
         .get();
   }
 
+  Stream<QuerySnapshot> streamMessagesForChat(String _chatID) {
+    return _db
+        .collection(DBCollections.chatCollection)
+        .doc(_chatID)
+        .collection(DBCollections.messagesCollection)
+        .orderBy(MessageFields.sentTime, descending: true)
+        .snapshots();
+  }
+
+  Future<void> addMessageToChat(String _chatID, ChatMessage _message) async {
+    try {
+      await _db
+          .collection(DBCollections.chatCollection)
+          .doc(_chatID)
+          .collection(DBCollections.messagesCollection)
+          .add(
+            _message.toJson(),
+          );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> updateChatData(
+      String _chatID, Map<String, dynamic> _data) async {
+    try {
+      await _db
+          .collection(DBCollections.chatCollection)
+          .doc(_chatID)
+          .update(_data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   Future<void> updateUserLastSeenTime(String _uid) async {
     try {
       await _db
@@ -54,6 +92,14 @@ class DatabaseService {
           .update({UserFields.lastActive: DateTime.now().toUtc()});
     } catch (e) {
       print(e.toString());
+    }
+  }
+
+  Future<void> deleteChat(String _chatID) async {
+    try {
+      await _db.collection(DBCollections.chatCollection).doc(_chatID).delete();
+    } catch (e) {
+      print(e);
     }
   }
 }
